dfdf60d2cb163cad61008ee0013083c081e98def
Nick <cnick1@vt.edu>
2019-05-12 17:11:24 -0400

    Started error plotting
    Formed error function and began plotting it

--- coupledWaveEquation/acousticPropagation2ndOrder.m
+++ coupledWaveEquation/acousticPropagation2ndOrder.m
@@ -32,39 +32,38 @@
 %% Formulating the system matrices $\mathbf{K}$ and $\mathbf{B}$
 % Define grid size and spacing (square for now)
 clear; close all; clc;
+animate=0; %Set to 0 to save time when doing comments
+
 rho=1.2; c=345;
 % rho=-1; c=1;
 
-n=40; m=n;
+xdim=40; ydim=xdim;
 dx=.25; dy=dx;
-fullDim=m*n;
-
+n=xdim*ydim;
 %%
 % Populate the coefficient matrix $\mathbf{K}$, i.e. the discretized
 % laplacian operator. It has an almost toeplitz form, with some extra zeros
 % on the off diagonal for boundary element conditions.
 
-row=[-2/dx^2-2/dy^2 1/dy^2 zeros(1, m-2) 1/dx^2 zeros(1,fullDim-(m+1))];
+row=[-2/dx^2-2/dy^2 1/dy^2 zeros(1, ydim-2) 1/dx^2 zeros(1,n-(ydim+1))];
 K=-rho * c^2 * toeplitz(row);
 
 %%
 % Set boundary element contributions to zero (artifact from making pressure
 % matrix into a vector for state) (zero gives Dirichlet aka mirror BC).
-for i = 1:n-1 
-    K(i*n+1,i*n)=0;
-    K(i*n,i*n+1)=0;
+for i = 1:xdim-1 
+    K(i*xdim+1,i*xdim)=0;
+    K(i*xdim,i*xdim+1)=0;
 end
 
 %plot(svd(K))
-
-C=eye(m*n);
 
 %%
 % We also define our initial condition $\mathbf{B=p0}$, where
 % $\mathbf{u}(t)$ is an impulse.
 
-xx=1:1:n;
-yy=1:1:n;
+xx=1:1:xdim;
+yy=1:1:xdim;
 parab_x0=20;
 parab_y0=parab_x0;
 %choose parabHeight = height of parabola
@@ -73,9 +72,9 @@ parabHeight=.05;
 parabRadius=10; % how many grid space for the radius of the parabola
 beta=parabHeight/(parabRadius*dy)^2;
 alfa=beta;
-ff=zeros(n);
-for i=1:n
-    for j=1:n
+ff=zeros(xdim);
+for i=1:xdim
+    for j=1:xdim
         if -alfa*(xx(i) -parab_x0)^2-beta*(yy(j)-parab_y0)^2+parabHeight >= 0
         ff(i,j)=-alfa*(xx(i) -parab_x0)^2-beta*(yy(j)-parab_y0)^2+parabHeight; 
         end
@@ -86,17 +85,19 @@ B=ff(:);
 %% Solve the full model using ODE45 as a baseline
 % Form the full A matrix and integrate using ODE45
 % 
-runFull=0;
-if runFull
-    A=[zeros(n^2) eye(n^2);
-        -K zeros(n^2)];
-    
-    x0=zeros(1,2*m*n);
-    x0(1:m*n)=B;
-    
-    tspan = [0 0.01];
-    [t, p] = ode45(@(t,p) myfun(t,p,A), tspan, x0);
-end
+
+A=[zeros(xdim^2) eye(xdim^2);
+    -K zeros(xdim^2)];
+
+x0=zeros(1,2*n);
+x0(1:n)=B;
+
+tf=0.02;
+tspan = [0 tf];
+[t, x] = ode45(@(t,p) myfun(t,p,A), tspan, x0);
+
+fullP=x(:,1:n);
+
 
 %% Form Galerkin reduction basis $\mathbf{V_r}$
 % We form reduction basis $\mathbf{V_r}$ for the system, given by an
@@ -105,11 +106,11 @@ end
 % $\mathbf{V_i} = (s_i^2 \mathbf{I + K})^{-1} \mathbf{B}$
 %
 
-r=5^2;
+r=16^2;
 si=logspace(1,5,r);
 
-Vi=zeros(m*n,r);
-I = eye(m*n);
+Vi=zeros(n,r);
+I = eye(n);
 for i=1:r
     Vi(:,i)=(si(i)^2*I+K)\B;
 end
@@ -142,31 +143,45 @@ Ar=[zeros(r) eye(r);
 xr0=zeros(1,2*r);
 xr0(1:r)=Br;
 
-runReduced=0;
-    if runReduced
-    tspan = [0 .01];
-    [t, pr] = ode45(@(t,pr) myfun(t,pr,Ar), tspan, xr0);
-end
+tspan = [0 tf];
+[t, xr] = ode45(@(t,pr) myfun(t,pr,Ar), tspan, xr0);
 
+% Project back to full space
+reducedP=(Vr*xr(:,1:r)')';
+
+% Interpolate reducedP to match the dimension of fullP
+reducedPinterp=interp1(reducedP,linspace(1,size(reducedP,1),size(fullP,1)));
+
+errorP=fullP-reducedPinterp;
+
+%% Plot error for particular node
+%
+figure
+plot(errorP(:,625))
+hold on
+plot(reducedPinterp(:,625))
+plot(fullP(:,625))
 %% Plot the animations
 %
-if runFull==1
-    fullP=p(:,1:m*n);
 
+if animate    
     for k = 1:size(fullP,1)
-        surf(vectomat(fullP(k,:),m,n));
-        axis([0 m 0 n -.1 .1])
+        surf(vectomat(fullP(k,:)-reducedPinterp(k,:),ydim,xdim));
+        axis([0 ydim 0 xdim -.1 .1])
+        drawnow;
+        errorFrames(k) = getframe;
+    end
+    
+    for k = 1:size(fullP,1)
+        surf(vectomat(fullP(k,:),ydim,xdim));
+        axis([0 ydim 0 xdim -.1 .1])
         drawnow;
         Mframes(k) = getframe;
     end
-end
 
-if runReduced
-    reducedP=Vr*pr(:,1:r)';
-
-    for k = 1:size(reducedP, 2)
-        surf(vectomat(reducedP(:,k),m,n));
-        axis([0 m 0 n -.1 .1])
+    for k = 1:size(reducedPinterp, 1)
+        surf(vectomat(reducedPinterp(k,:),ydim,xdim));
+        axis([0 ydim 0 xdim -.1 .1])
         drawnow;
         Mrframes(k) = getframe;
     end
